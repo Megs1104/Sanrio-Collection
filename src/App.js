b10731d1c5ae9f8e3911620068a5d031c3e296a4
Megs1104 <megangilling1@icloud.com>
2026-01-30 17:04:54 +0000

    Refactoring to connect to AWS backend using DynamoDB, Lambda and API gateway

--- src/App.js
+++ src/App.js
@@ -1,15 +1,47 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import CharacterGrid from './components/CharacterGrid';
 import './App.css';
-import { characters } from './data/characters.js';
+
 
 function App(){
+  const [characters, setCharacters] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  useEffect(() => {
+    fetch('https://1ah3etfl1l.execute-api.eu-west-2.amazonaws.com/getCharacters')
+    .then(response => response.json())
+    .then(data => {
+      setCharacters(data);
+      setLoading(false);
+    })
+    .catch(error => {
+      console.error('Error fetching characters:', error);
+      setLoading(false);
+    })
+  }, []);
+
+  const filteredCharacters = characters.filter(char => char.name.toLowerCase().includes(searchTerm.toLowerCase()));
+
+  if (loading){
+    return <div className="App">
+      <h1>Loading...</h1>
+    </div>
+  }
+
   return (
     <div className="App">
       <h1>Sanrio Collection</h1>
+      <input
+      type="text"
+      placeholder="Search characters..."
+      value={searchTerm}
+      onChange={(e) => setSearchTerm(e.target.value)
+      }
+      />
       <CharacterGrid characters={characters}/>
     </div>
-  )
+  );
 };
 
 export default App;
